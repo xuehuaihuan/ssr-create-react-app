3258da729e1694bb0711619129b7fbc24edb8b06
Huaihuan Xue <xuehuaihuan@163.com>
2019-12-05 19:02:45 +0800

    save restart server tips - 保存重启服务器提示

--- server/index.js
+++ server/index.js
@@ -4,11 +4,20 @@ import fs from 'fs';
 import { getStyle } from './style';
 import { handleStatic } from './static';
 import { resolveServer } from './paths';
+import { DEVELOPMENT } from './constant';
+
+// import openBrowser from 'react-dev-utils/openBrowser';
+import { prepareUrls } from 'react-dev-utils/WebpackDevServerUtils';
+import chalk from 'react-dev-utils/chalk';
 
 const { styleTags, appHtml } = getStyle();
 
+const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+const HOST = process.env.HOST || '0.0.0.0';
 const REACT_APP_PORT = process.env.REACT_APP_PORT;
 const port = REACT_APP_PORT || 3030;
+const NODE_ENV = process.env.NODE_ENV;
+const urls = prepareUrls(protocol, HOST, port);
 
 const app = express();
 
@@ -30,3 +39,22 @@ app.get(
 );
 
 app.listen(port, () => console.log('app started at port:' + port));
+
+if (NODE_ENV === DEVELOPMENT) {
+  if (urls.lanUrlForTerminal) {
+    console.log(
+      `
+        ${chalk.bold('Local:')}            ${urls.localUrlForTerminal}`,
+    );
+
+    console.log(
+      `
+        ${chalk.bold('On Your Network:')}  ${urls.lanUrlForTerminal}
+      `,
+    );
+  } else {
+    console.log(`${urls.localUrlForTerminal}`);
+  }
+
+  // openBrowser((urls.localUrlForBrowser));
+}
